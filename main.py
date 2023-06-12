bd2d776061b61060c72aec6121440547b296f8fb
= <=>
2023-06-12 15:36:34 +0100

    full

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import sys, csv, requests 
 from bs4 import BeautifulSoup
 
-# page = requests.get("https://civlcomps.org/event/pga-worlds-2023/participants?ParticipantSearch%5Bgroup%5D=&ParticipantSearch%5Bgroup%5D=gender&ParticipantSearch%5Bstatus%5D=")
-# soup = BeautifulSoup(page.content, 'html.parser')
-# pilotJson = []
-# table = soup.table
-# rows = table.find_all("tr")
-# rows.pop(0)
-# for row in rows:
-#     country = row.find(attrs={"data-col":"ioc"}).get("data-value")
-#     pilotJson.append([country,"M"])
+page = requests.get("https://civlcomps.org/event/pga-worlds-2023/participants?ParticipantSearch%5Bgroup%5D=&ParticipantSearch%5Bgroup%5D=gender&ParticipantSearch%5Bstatus%5D=")
+soup = BeautifulSoup(page.content, 'html.parser')
+pilotJson = []
+table = soup.table
+rows = table.find_all("tr")
+rows.pop(0)
+for row in rows:
+    country = row.find(attrs={"data-col":"ioc"}).get("data-value")
+    pilotJson.append([country,"M"])
 
-# table.decompose()
-# table = soup.table
-# rows = table.find_all("tr")
-# rows.pop(0)
-# for row in rows:
-#     country = row.find(attrs={"data-col":"ioc"}).get("data-value")
-#     pilotJson.append([country,"F"])
+table.decompose()
+table = soup.table
+rows = table.find_all("tr")
+rows.pop(0)
+for row in rows:
+    country = row.find(attrs={"data-col":"ioc"}).get("data-value")
+    pilotJson.append([country,"F"])
 
-# with open('registered pilots.csv', 'w', newline='') as file:
-#      writer = csv.writer(file)
-#      writer.writerows(pilotJson)
+with open('registered pilots.csv', 'w', newline='') as file:
+     writer = csv.writer(file)
+     writer.writerows(pilotJson)
 
 
      
@@ -34,7 +34,7 @@ for i, row in enumerate(rows):
     # country = row.select(".col-2")[1].text
     country = row.find_all("div", {"class": "col-2"})[1].text.strip()
     nations.append([i+1, country])
-print(nations)
+# print(nations)
 
 with open('countries.csv', 'w', newline='') as file:
      writer = csv.writer(file)
