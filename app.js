6ab4843322b16dd52050fdfb3e6ca39e6c0e5f0a
= <=>
2023-06-12 15:36:16 +0100

    added

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 document.getElementById("submit").addEventListener("click", run);
-
+frame = document.getElementById("frame");
+frame.onload = function() {scrap()};
 function run() {
     url = document.getElementById("url").value;
-    console.log(url);
+    // console.log(url);
+    // frame.src=url;
+}
+
+function scrap(){
+    tables = frame.contentWindow.document.querySelectorAll("table");
+    console.log(tables);
 }
